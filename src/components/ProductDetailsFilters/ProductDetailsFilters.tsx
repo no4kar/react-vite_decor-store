21e67bf91b56182f6ed5046e9695d6b372046315
Bahtijar <no4kar@gmail.com>
2024-04-29 11:24:47 +0200

    add:ProductDetailsFilters as a separate component improvment:productDetails.page

---
+++ src/components/ProductDetailsFilters/ProductDetailsFilters.tsx
@@ -0,0 +1,172 @@
+import * as R from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { TyProduct } from '../../types/Products/Products';
+import DropdownMultiSelect from '../DropdownMultiSelect/DropdownMultiSelect';
+import {
+  SearchParams, SearchParamsName, getSearchWith,
+} from '../../helpers/searchHelper';
+
+export const ProductDetailsFilters = R.memo(
+  ({
+    products,
+    onClose,
+  }: {
+    products: TyProduct[];
+    onClose?: () => void;
+  }) => {
+    console.log('render');
+    const [searchParams, setSearchParams] = useSearchParams();
+    const setSearchWith = R.useCallback((params: SearchParams) => {
+      setSearchParams(getSearchWith(searchParams, params));
+    }, [setSearchParams, searchParams]);
+
+    const selectedCountries
+      = searchParams.getAll(SearchParamsName.COUNTRY);
+    const selectedCollections
+      = searchParams.getAll(SearchParamsName.COLLECTION);
+    const selectedProducers
+      = searchParams.getAll(SearchParamsName.PRODUCER);
+
+    const productOptions = R.useMemo(
+      () => products.reduce((a, c) => {
+        a[SearchParamsName.COUNTRY].add(c[SearchParamsName.COUNTRY]);
+        a[SearchParamsName.COLLECTION].add(c[SearchParamsName.COLLECTION]);
+        a[SearchParamsName.PRODUCER].add(c[SearchParamsName.PRODUCER]);
+
+        return a;
+      }, {
+        [SearchParamsName.COUNTRY]: new Set<string>(),
+        [SearchParamsName.COLLECTION]: new Set<string>(),
+        [SearchParamsName.PRODUCER]: new Set<string>(),
+      }),
+      [products],
+    );
+
+    const productCountries = R.useMemo(
+      () => Array.from(productOptions[SearchParamsName.COUNTRY])
+        .map((item) => ({ value: item, content: item, })),
+      [productOptions],
+    );
+
+    const productCollections = R.useMemo(
+      () => Array.from(productOptions[SearchParamsName.COLLECTION])
+        .map((item) => ({ value: encodeURIComponent(item), content: item, })),
+      [productOptions],
+    );
+
+    const productProducers = R.useMemo(
+      () => Array.from(productOptions[SearchParamsName.PRODUCER])
+        .map((item) => ({ value: encodeURIComponent(item), content: item, })),
+      [productOptions],
+    );
+
+    const handleCountryChange = (value: string[]): void => {
+      setSearchWith({
+        [SearchParamsName.COUNTRY]: value || null,
+      });
+    };
+
+    const handleCollectionChange = (value: string[]): void => {
+      setSearchWith({
+        [SearchParamsName.COLLECTION]: value || null,
+      });
+    };
+
+    const handleProducerChange = (value: string[]): void => {
+      setSearchWith({
+        [SearchParamsName.PRODUCER]: value || null,
+      });
+    };
+
+    return (
+      <div
+        className="
+      h-full w-[320px]
+      flex flex-col
+      bg-white border border-red-300 border-solid
+      "
+      >
+        {onClose && (
+          <div
+            className="
+        flex justify-end
+        h-fit
+        border border-red-300 border-solid
+        ">
+            <button
+              type="button"
+              aria-label="filter"
+              className="
+          w-[50px] h-[50px]
+          flex justify-center items-center
+          border border-gray-100 border-solid
+          transform transition duration-300 hover:scale-105 active:scale-100
+          "
+              onClick={onClose}
+            >
+              <i className="icon icon--filter" />
+            </button>
+          </div>
+        )}
+
+        <ul
+          className="
+          px-[24px] pt-[8px]
+          "
+        >
+          <li>
+            <DropdownMultiSelect
+              placeholder={
+                <div className="flex gap-[8px]">
+                  <img
+                    src="./icons/globe-01.svg"
+                    alt="globe-01.svg"
+                  />
+                  <p>Countries</p>
+                </div>
+              }
+              options={productCountries}
+              selectedOptions={selectedCountries}
+              onChange={handleCountryChange}
+            />
+          </li>
+
+          <li>
+            <DropdownMultiSelect
+              placeholder={
+                <div className="flex gap-[8px]">
+                  <img
+                    src="./icons/grid-01.svg"
+                    alt="grid-01.svg"
+                  />
+                  <p>Collections</p>
+                </div>
+              }
+              options={productCollections}
+              selectedOptions={selectedCollections}
+              onChange={handleCollectionChange}
+            />
+          </li>
+
+          <li>
+            <DropdownMultiSelect
+              placeholder={
+                <div className="flex gap-[8px]">
+                  <img
+                    src="./icons/factory-building.svg"
+                    alt="factory-building.svg"
+                  />
+                  <p>Producers</p>
+                </div>
+              }
+              options={productProducers}
+              selectedOptions={selectedProducers}
+              onChange={handleProducerChange}
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+);
--- src/pages/products.page/products.page.tsx
+++ src/pages/products.page/products.page.tsx
@@ -7,14 +7,13 @@ import { PageNavigation } from '../../components/PageNavigation';
 import { Search } from '../../components/Search';
 
 import {
-  SearchParams, SearchParamsName, getSearchWith
+  SearchParamsName
 } from '../../helpers/searchHelper';
 import ProductCard2 from '../../components/ProductCard2/ProductCard2';
-import DropdownMultiSelect
-  from '../../components/DropdownMultiSelect/DropdownMultiSelect';
 import { TyProduct } from '../../types/Products/Products';
 
 import './products.page.scss';
+import { ProductDetailsFilters } from '../../components/ProductDetailsFilters';
 
 export const ProductsPage = ({
   products,
@@ -27,10 +26,8 @@ export const ProductsPage = ({
 }) => {
   console.log('render');
   const [isAsideOpen, setIsAsideOpen] = R.useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const setSearchWith = R.useCallback((params: SearchParams) => {
-    setSearchParams(getSearchWith(searchParams, params));
-  }, [setSearchParams, searchParams]);
+  const [searchParams] = useSearchParams();
+  const [sortByPriceInc, setSortByPriceInc] = R.useState<boolean>(true);
 
   const query
     = (searchParams.get(SearchParamsName.QUERY) || '').toLocaleLowerCase();
@@ -40,39 +37,6 @@ export const ProductsPage = ({
     = searchParams.getAll(SearchParamsName.COLLECTION);
   const selectedProducers
     = searchParams.getAll(SearchParamsName.PRODUCER);
-
-  const productOptions = R.useMemo(
-    () => products.reduce((a, c) => {
-      a[SearchParamsName.COUNTRY].add(c[SearchParamsName.COUNTRY]);
-      a[SearchParamsName.COLLECTION].add(c[SearchParamsName.COLLECTION]);
-      a[SearchParamsName.PRODUCER].add(c[SearchParamsName.PRODUCER]);
-
-      return a;
-    }, {
-      [SearchParamsName.COUNTRY]: new Set<string>(),
-      [SearchParamsName.COLLECTION]: new Set<string>(),
-      [SearchParamsName.PRODUCER]: new Set<string>(),
-    }),
-    [products],
-  );
-
-  const productCountries = R.useMemo(
-    () => Array.from(productOptions[SearchParamsName.COUNTRY])
-      .map((item) => ({ value: item, content: item, })),
-    [productOptions],
-  );
-
-  const productCollections = R.useMemo(
-    () => Array.from(productOptions[SearchParamsName.COLLECTION])
-      .map((item) => ({ value: encodeURIComponent(item), content: item, })),
-    [productOptions],
-  );
-
-  const productProducers = R.useMemo(
-    () => Array.from(productOptions[SearchParamsName.PRODUCER])
-      .map((item) => ({ value: encodeURIComponent(item), content: item, })),
-    [productOptions],
-  );
 
   const visibleProducts = products
     .filter((p) => (
@@ -89,186 +53,137 @@ export const ProductsPage = ({
     .filter((p) => {
       return `${p.id}/${p.producer}/${p.collection}/${p.country}`
         .toLocaleLowerCase().includes(query);
+    })
+    .sort((a, b) => {
+      return (a.price - b.price) * (sortByPriceInc ? 1 : -1);
     });
 
-  const handleCountryChange = (value: string[]): void => {
-    setSearchWith({
-      [SearchParamsName.COUNTRY]: value || null,
-    });
-  };
-
-  const handleCollectionChange = (value: string[]): void => {
-    setSearchWith({
-      [SearchParamsName.COLLECTION]: value || null,
-    });
-  };
-
-  const handleProducerChange = (value: string[]): void => {
-    setSearchWith({
-      [SearchParamsName.PRODUCER]: value || null,
-    });
-  };
+  console.info(visibleProducts.length);
 
   return (
-    <div className="Wallpaper relative min-h-screen">
+    <div className="Wallpaper
+    relative min-h-screen
+    border border-blue-300 border-solid
+    "
+    >
       <aside
         id="sidebar"
         className={cn(
           'absolute top-0 left-0 bottom-0 right-0 z-10',
-          'flex flex-col',
-          'h-full w-[320px]',
-          'bg-white',
-          'border border-red-500 border-solid',
+          'border border-red-300 border-solid',
           'pointer-events-none',
           'transform -translate-x-full',
           'transition-transform',
           'duration-1000',
+          'md:hidden',
           {
             'transform translate-x-0 pointer-events-auto': isAsideOpen,
           }
         )}
       >
-        <div className="
-        flex justify-end
-        h-fit
-        border border-red-500 border-solid
-        ">
-          <button
-            type="button"
-            aria-label="filter"
-            className="
-          w-[50px] h-[50px]
-          flex justify-center items-center
-          border border-gray-100 border-solid
-          hover:scale-110
-          "
-            onClick={() => setIsAsideOpen(false)}
-          >
-            <i className="icon icon--filter" />
-          </button>
-        </div>
-
-        <ul
-          className="
-          px-[24px] pt-[8px]
-          "
-        >
-          <li>
-            <DropdownMultiSelect
-              placeholder={
-                <div className="flex gap-[8px]">
-                  <img
-                    src="./icons/globe-01.svg"
-                    alt="globe-01.svg"
-                  />
-                  <p>Countries</p>
-                </div>
-              }
-              options={productCountries}
-              selectedOptions={selectedCountries}
-              onChange={handleCountryChange}
-            />
-          </li>
-
-          <li>
-            <DropdownMultiSelect
-              placeholder={
-                <div className="flex gap-[8px]">
-                  <img
-                    src="./icons/grid-01.svg"
-                    alt="grid-01.svg"
-                  />
-                  <p>Collections</p>
-                </div>
-              }
-              options={productCollections}
-              selectedOptions={selectedCollections}
-              onChange={handleCollectionChange}
-            />
-          </li>
-
-          <li>
-            <DropdownMultiSelect
-              placeholder={
-                <div className="flex gap-[8px]">
-                  <img
-                    src="./icons/factory-building.svg"
-                    alt="factory-building.svg"
-                  />
-                  <p>Producers</p>
-                </div>
-              }
-              options={productProducers}
-              selectedOptions={selectedProducers}
-              onChange={handleProducerChange}
-            />
-          </li>
-        </ul>
+        <ProductDetailsFilters
+          products={products}
+          onClose={() => setIsAsideOpen(false)}
+        />
       </aside>
 
-      <div className={cn('content flex flex-col gap-[40px]', {
-        'pointer-events-none': isAsideOpen,
-      })}>
+      <div className={cn('content flex flex-col gap-[40px]',
+        'border border-red-300 border-solid',
+        {
+          'pointer-events-none': isAsideOpen,
+        })}>
         <PageNavigation />
         {isLoading && <Loader />}
 
         <div className="
+        flex flex-col gap-[40px]
+        md:flex-row md:gap-[130px]"
+        >
+          <div className="
         w-full
         h-[44px]
         border
         border-accent
         border-solid
+        md:flex-1
         ">
-          <Search placeholder="Введіть назву або код товару" />
-        </div>
-
-        <div className="
-        w-full h-[44px]
-        flex justify-between sm:justify-evenly
-        border border-red-500 border-solid
-        ">
-          <button
-            type="button"
-            aria-label="filter"
-            className="
-          w-[50px] h-full
-          flex justify-center items-center
-          border border-red-500 border-solid
-          hover:scale-110
-          "
-            onClick={() => setIsAsideOpen(true)}
-          >
-            <i className="icon icon--filter" />
-          </button>
+            <Search placeholder="Введіть назву або код товару" />
+          </div>
 
           <div className="
-          h-full
-          flex items-center
-          border border-red-500 border-solid
-          ">
-            <p className="text-black uppercase">ціна за зростанням</p>
+        w-full h-[44px]
+        flex gap-[20px]
+        sm:justify-evenly
+        border border-red-300 border-solid
+        md:w-[310px]
+        ">
+            <button
+              type="button"
+              aria-label="filter"
+              className="
+          w-[50px] h-full
+          flex justify-center items-center
+          border border-red-300 border-solid
+          transform transition duration-300 hover:scale-105 active:scale-100
+          md:hidden
+          "
+              onClick={() => setIsAsideOpen(true)}
+            >
+              <i className="icon icon--filter" />
+            </button>
+
+            <button
+              type="button"
+              aria-label="sortByPrice"
+              className="
+          h-full px-[17px]
+          flex-1 flex justify-between items-center
+          border border-red-300 border-solid
+          transform transition duration-300 hover:scale-105 active:scale-100
+          sm:flex-none sm:gap-[10px]
+          md:w-full
+          "
+              onClick={() => setSortByPriceInc(prev => !prev)}
+            >
+              <i className="icon icon--filter" />
+              <p className="text-black uppercase">ціна за зростанням</p>
+            </button>
           </div>
         </div>
 
+
         {!isLoading && hasError && <p>{hasError}</p>}
 
-        {!isLoading && !hasError && (
-          <div
+        <div className='flex'>
+          <aside
+            id="sidebar"
             className={cn(
-              'grid',
-              'grid-cols-1',
-              'gap-[24px]',
-              'sm:grid-cols-2',
-              'md:grid-cols-4',
+              'hidden',
+              'border border-red-300 border-solid',
+              'md:block',
             )}
           >
-            {visibleProducts.map((product) => (
-              <ProductCard2
-                key={product.id}
-                product={product} />
-            ))}
-          </div >
-        )}
+            <ProductDetailsFilters
+              products={products}
+            />
+          </aside>
+
+          {!isLoading && !hasError && (
+            <div
+              className="
+              flex-1
+              grid grid-cols-1 gap-[24px]
+              sm:grid-cols-2
+              md:grid-cols-3"
+            >
+              {visibleProducts.map((product) => (
+                <ProductCard2
+                  key={product.id}
+                  product={product} />
+              ))}
+            </div >
+          )}
+        </div>
       </div>
     </div >
   );
